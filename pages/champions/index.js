cb1cff04a045890413fbd46d4aafba2536a42f27
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-11-21 22:16:58 +0300

    [DEV 2] - update

--- pages/champions/index.js
+++ pages/champions/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import ChampionSmall from "../../components/ChampionSmall/ChampionSmall";
 import { getChampions } from "../../utils/api/champion";
 import styles from "./champions.module.scss";
@@ -10,8 +12,16 @@ export default function Home({ champions }) {
     // for (const [key, value] of Object.entries(champions.data)) {
     //     content.push(<h1>{JSON.stringify(value)} <hr></hr></h1>)
     // }
+  function loadMoreContent(){
+    setloadedChamp(prevValue => prevValue + 20);
+    setChampData(content.slice(0, loadedChamp));
+  }
+  const [loadedChamp, setloadedChamp] = useState(20)
+  const [champData, setChampData] = useState(content.slice(0,loadedChamp));
   return (
     <div className={styles.container}>
+      {/* {champData}
+      <button onClick={loadMoreContent}>More</button> */}
       {content}
     </div>
   );

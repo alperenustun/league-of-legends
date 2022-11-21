62a7820dbd021ed47358bba84ee897c9f174a8cb
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-11-22 01:47:09 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -23,13 +23,19 @@ export default function Home({champion}) {
           <div className={styles.championsSectionText}>
             <h1>League of Legends Evrenini Keşfedin</h1>
             <p>
-              Pellentesque habitant morbi tristique senectus et netus et
-              malesuada fames ac turpis egestas. Cras gravida nibh vel hendrerit
-              fringilla. Pellentesque non est elementum, posuere quam nec,
-              posuere arcu. Fusce vulputate pretium dui, a molestie tellus.
-              Integer tempor eros in consequat pellentesque. Curabitur tincidunt
-              quis orci in ultrices. Pellentesque habitant morbi tristique
-              senectus et netus et malesuada fames ac turpis egestas.
+              Yüzyıllar önce, &apos;Dünya Rünleri&apos; adı verilen büyülü eserler yeniden
+              keşfedildi. Sonraki on yıllar boyunca, Rünler hakkındaki bilgi,
+              daha fazla şey ortaya çıktıkça yayılmaya başladı. Dünyanın en
+              parlak beyinleri, sahip oldukları güçleri belirlemeye çalışarak
+              eski glifleri inceledi. Hatta çok azı kökenlerinin önemini veya
+              içlerinde barındırdıkları katıksız gücü anlamaya başlayabilirdi.
+              Bazıları Rünlerin Runeterra&apos;nın yaratılmasının ayrılmaz bir
+              parçası olduğunu düşündü. Bu gizemli eserlerin ilk kullanımı, tüm
+              ulusların manzarasını yeniden şekillendirdikleri için felaketle
+              sonuçlandı. Rünleri bilenler bu tür &quot;Yapıcıların Kudreti&quot;nin bir
+              silah olarak kullanıldığını hayal ettikçe, güvensizlik hızla
+              arttı. Rün Savaşları olarak bilinen bu büyülü eserlerle ilgili
+              çatışmalar çıkmaya başladı.
             </p>
             <button className={styles.championsSectionButton}>Şampiyonlar</button>
           </div>
